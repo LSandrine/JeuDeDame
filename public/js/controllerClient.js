c2d54b7b1ad413b3837fae6827914a797c69fea0
LSandrine <latour.sandrine.he@gmail.com>
2017-02-17 16:13:21 +0100

    eparation du git

--- public/js/controllerClient.js
+++ public/js/controllerClient.js
@@ -1,19 +1,13 @@
 $(document).ready(function(){
   connexion();
-<<<<<<< HEAD
   $('body').on("click","#startPartie",afficherPlateau);
-=======
->>>>>>> 3de8906633dfd81a5fb8965281e51b9e383b7b15
 });
 function controllerLobby(){
   $('#contenu').html(lobby());
   $('title').text('lobby jeu de dame');
 }
-<<<<<<< HEAD
 
 function afficherPlateau(){
   $('#contenu').html(plateau());
   $('title').text('Plateau du jeu');
 }
-=======
->>>>>>> 3de8906633dfd81a5fb8965281e51b9e383b7b15
